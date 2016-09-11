63f01319f5abf5ed142b006f264a7985216bd7f8
Patrick <pattyboy_07@hotmail.com>
2016-09-11 16:40:54 +1200

    working resize

--- test.js
+++ test.js
@@ -1,7 +1,63 @@
 $(document).ready(function() {
-	for (var i = 1; i <= 10; i++) {
-		for (var j = 1; j <= 10; j++) {
+	squareNum = 16;
+	for (var i = 1; i <= squareNum; i++) {
+		for (var j = 1; j <= squareNum; j++) {
 			$('#container').append('<div class="pixel"></div>')
 		};
 	}
+
+	$("div").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "black");
 });
+
+
+	$("#start").click(function() {
+  squareNum = prompt("How many squares would you like?", "16");
+  
+  var squareSize = 960/squareNum;
+  console.log(squareNum);
+  console.log(squareSize);
+	
+   $(".pixel").remove();
+
+for (var i = 1; i <= squareNum; i++) {
+		for (var j = 1; j <= squareNum; j++) {
+			$('#container').append('<div class="pixel"></div>')
+		};
+	}
+
+	$(".pixel").css("height", squareSize+"px");
+	$(".pixel").css("width", squareSize+"px");
+
+
+	$("div").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "black");
+});
+	
+	//location.reload();
+});
+
+	$("#clear").click(function() {
+	
+   $(".pixel").remove();
+
+for (var i = 1; i <= squareNum; i++) {
+		for (var j = 1; j <= squareNum; j++) {
+			$('#container').append('<div class="pixel"></div>')
+		};
+	}
+
+	$("div").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "black");
+});
+	
+	//location.reload();
+});
+});
+
